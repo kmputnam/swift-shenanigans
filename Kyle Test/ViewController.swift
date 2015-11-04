18882ddf19fa3c2e55074c1e100add2c865e98b6
Kyle Putnam <kylemputnam@gmail.com>
2015-11-03 19:44:41 -0600

    Initial Commit

---
+++ Kyle Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kyle Test
+//
+//  Created by Kyle Putnam on 11/3/15.
+//  Copyright © 2015 Kyle Putnam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
